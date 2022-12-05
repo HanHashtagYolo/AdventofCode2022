9fe8aa37925ae27a178d732f891b5a79d8d80ae2
HanHashtagYolo <75455776+HanHashtagYolo@users.noreply.github.com>
2022-12-05 13:47:50 -0700

    AoC Day 5
    Solutions and Input for Advent of Code Day 5

---
+++ Day5Part1.py
@@ -0,0 +1,50 @@
+import os
+
+stacks = {}
+def doMoves(numFromTo):
+	global stacks
+	num = numFromTo[0]
+	From = numFromTo[1]
+	To = numFromTo[2]
+	for i in range(0,num):
+		stacks[To].append(stacks[From][-1])
+		stacks[From].pop()
+
+file = open("Day5Input.txt")
+text = file.read()
+stack, moves = text.split('\n\n')
+
+stack = stack.split('\n')
+moves = moves.split('\n')
+
+lastNum = int(stack[-1].strip().split(' ')[-1])
+
+for i in range(1,lastNum+1):
+	stacks[i] = []
+
+for j in range(0,len(stack)-1):
+	for k in range(0,len(stack[j]),4):
+		if '[' in stack[j][k:k+3]:
+			stacks[(k/4)+1].append(stack[j][k+1])
+for item in stacks:
+	stacks[item].reverse()
+
+moveList = []
+for item in moves:
+	numFromTo = []
+	temp = item.split(' ')
+	numFromTo.append(int(temp[1]))
+	numFromTo.append(int(temp[3]))
+	numFromTo.append(int(temp[5]))
+	moveList.append(numFromTo)
+
+for move in moveList:
+	doMoves(move)
+
+output = ''
+for item in stacks:
+	output = output+stacks[item][-1]
+
+print output
+
+#for each move append last value to new location, then pop from old location
